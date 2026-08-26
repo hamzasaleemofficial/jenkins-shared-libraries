ceeb27c184a75e6324145be1ec36030458258a08
Hamza Saleem <60598687+hamzasaleemofficial@users.noreply.github.com>
2026-08-26 21:58:57 +0400

    Refactor docker push script for improved readability

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,9 +1,7 @@
  def call(String ProjectName, String ImageTag, String dockerHubUser){
-   withCredentials(
-     [usernamePassword(credentialsId:'dockerHubCred',usernameVariable:'dockerHubUser', passwordVariable:'dockerHubPass')])
+   withCredentials([usernamePassword(credentialsId:'dockerHubCred',usernameVariable:'dockerHubUser', passwordVariable:'dockerHubPass')])
    {
                 sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
-               
-                sh 'docker push ${dockerHubUser}/${ProjectName}:${ImageTag}'
-  }
+   }
+                 sh 'docker push ${dockerHubUser}/${ProjectName}:${ImageTag}'
  }
