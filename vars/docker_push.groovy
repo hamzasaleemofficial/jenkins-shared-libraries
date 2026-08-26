a94823d2eaa41c992353b235c0a52908f8a9e54c
Hamza Saleem <60598687+hamzasaleemofficial@users.noreply.github.com>
2026-08-26 21:49:25 +0400

    Rename parameter 'Project' to 'ProjectName' in docker_push

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,9 +1,9 @@
- def call(String Project, String ImageTag, String dockerHubUser){
+ def call(String ProjectName, String ImageTag, String dockerHubUser){
    withCredentials(
      [usernamePassword(credentialsId:'dockerHubCred',usernameVariable:'dockerHubUser', passwordVariable:'dockerHubPass')])
    {
                 sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
                
-                sh 'docker push $dockerHubUser/$Project:$ImageTag'
+                sh 'docker push ${dockerHubUser}/${ProjectName}:${ImageTag}'
   }
  }
